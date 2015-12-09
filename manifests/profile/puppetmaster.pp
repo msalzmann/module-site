8fa986dd79035bba88c6834861dd5d1b83d18d1e
Matthias Salzmann <matthias.salzmann@id.unibe.ch>
2015-12-09 15:23:25 +0100

    add firewall

--- manifests/profile/puppetmaster.pp
+++ manifests/profile/puppetmaster.pp
@@ -8,15 +8,15 @@
 
 #### Firewall rules ####
 
-#  firewall { '100 allow puppetmaster foreman access':
-#   dport  => [ 22, 80, 443, 8140, 3000, 8443],
-#   proto  => tcp,
-#   action => accept,
-#  }
+  firewall { '100 allow puppetmaster foreman access':
+   dport  => [ 22, 80, 443, 8140, 3000, 8443],
+   proto  => tcp,
+   action => accept,
+  }
 
-#  firewall { '101 allow snmp access':
-#   dport => 161,
-#   proto => udp,
-#   action => accept,
-#  }
+  firewall { '101 allow snmp access':
+   dport => 161,
+   proto => udp,
+   action => accept,
+  }
 }
