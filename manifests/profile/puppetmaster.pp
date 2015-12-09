adff565368c97fc322dd858f7e6318eb1fe127cb
Matthias Salzmann <matthias.salzmann@id.unibe.ch>
2015-12-09 14:12:01 +0100

    remove firewall

--- manifests/profile/puppetmaster.pp
+++ manifests/profile/puppetmaster.pp
@@ -2,22 +2,21 @@
 ### Hier wird das Hiera File eingebunden
 
  class site::profile::puppetmaster {
-    include ::winbind
 
 
 
 
 #### Firewall rules ####
 
-  firewall { '100 allow puppetmaster foreman access':
-   dport  => [ 22, 80, 443, 8140, 3000, 8443],
-   proto  => tcp,
-   action => accept,
-  }
+#  firewall { '100 allow puppetmaster foreman access':
+#   dport  => [ 22, 80, 443, 8140, 3000, 8443],
+#   proto  => tcp,
+#   action => accept,
+#  }
 
-  firewall { '101 allow snmp access':
-   dport => 161,
-   proto => udp,
-   action => accept,
-  }
+#  firewall { '101 allow snmp access':
+#   dport => 161,
+#   proto => udp,
+#   action => accept,
+#  }
 }
